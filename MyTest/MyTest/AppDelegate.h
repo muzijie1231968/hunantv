e702953d884de0d825dd51457d7499b41fb97454
lj <252592451@qq.com>
2015-05-22 09:08:06 +0800

    初始化项目

---
+++ MyTest/MyTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyTest
+//
+//  Created by li jie on 15/5/22.
+//  Copyright (c) 2015年 sunshine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyTest/MyTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyTest
+//
+//  Created by li jie on 15/5/22.
+//  Copyright (c) 2015年 sunshine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
